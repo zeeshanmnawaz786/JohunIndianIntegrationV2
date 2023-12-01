edff911b8a2ad8cd3bb4a52023a4216f71448e05
zeeshanmnawaz786 <zeesahnm.nawaz786@gmail.com>
2023-12-01 21:48:27 +0500

    smart contract done

--- smartcontract/contracts/VehicleManagement.sol
+++ smartcontract/contracts/VehicleManagement.sol
@@ -1,4 +1,148 @@
-// SPDX-License-Identifier: MIT
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.0;
+
+// import "hardhat/console.sol";
+
+// contract VehicleManagement {
+//     address public owner;
+//     uint256 public userCounter;
+//     uint256 public vehicleCounter;
+//     string[] registerVehicleCounter;
+
+//     enum RequestStatus {
+//         InProgress,
+//         Accepted,
+//         Rejected
+//     }
+
+//     constructor() {
+//         owner = msg.sender;
+//     }
+
+//     struct User {
+//         bool registered;
+//     }
+
+//     struct Vehicle {
+//         string ownerName;
+//         string contact;
+//         string model;
+//         string color;
+//         string number;
+//         string category;
+//         string chassisNumber;
+//         address owner;
+//         RequestStatus status;
+//     }
+
+//     struct OwnershipRequest {
+//         string number;
+//         address newOwner;
+//         address requester;
+//     }
+
+//     mapping(address => User) public users;
+//     mapping(string => Vehicle) public vehicles;
+//     mapping(string => address[]) public ownershipHistory;
+
+//     modifier onlyOwner() {
+//         require(msg.sender == owner, "Only the owner can perform this action");
+//         _;
+//     }
+
+//     modifier onlyRegisteredUser() {
+//         require(users[msg.sender].registered, "User is not registered");
+//         _;
+//     }
+
+//     function registerUser() public {
+//         require(!users[msg.sender].registered, "User is already registered");
+//         users[msg.sender] = User(true);
+//         userCounter = userCounter + 1;
+//     }
+
+//     function requestVehicleRegistration(
+//         string memory _ownerName,
+//         string memory _contact,
+//         string memory _number,
+//         string memory _model,
+//         string memory _color,
+//         string memory _category,
+//         string memory _chassisNumber
+//     ) public onlyRegisteredUser {
+//         require(
+//             vehicles[_number].owner == address(0),
+//             "Vehicle with this number is already registered"
+//         );
+
+//         Vehicle memory newRequest = Vehicle({
+//             ownerName: _ownerName,
+//             contact: _contact,
+//             number: _number,
+//             model: _model,
+//             color: _color,
+//             category: _category,
+//             chassisNumber: _chassisNumber,
+//             owner: msg.sender,
+//             status: RequestStatus.InProgress
+//         });
+//         vehicles[_number] = newRequest;
+//         vehicleCounter = vehicleCounter + 1;
+//         registerVehicleCounter.push(_number);
+//     }
+
+//     function approveVehicleRegistration(
+//         string memory _number
+//     ) public onlyOwner {
+//         Vehicle storage request = vehicles[_number];
+//         require(
+//             request.owner != address(0),
+//             "No pending request for this vehicle"
+//         );
+
+//         vehicles[request.number].status = RequestStatus.Accepted;
+//     }
+
+//     function rejectVehicleRegistration(string memory _number) public onlyOwner {
+//         vehicles[_number].status = RequestStatus.Rejected;
+//     }
+
+//     function getAllVehicles() public view returns (Vehicle[] memory) {
+//         Vehicle[] memory allVehicles = new Vehicle[](
+//             registerVehicleCounter.length
+//         );
+
+//         for (uint256 i = 0; i < registerVehicleCounter.length; i++) {
+//             string memory requestNumber = registerVehicleCounter[i];
+//             Vehicle storage vehicle = vehicles[requestNumber];
+
+//             allVehicles[i] = vehicle;
+//         }
+
+//         return allVehicles;
+//     }
+
+//     function getOwnershipHistory(
+//         string memory _number
+//     ) public view returns (address[] memory) {
+//         return ownershipHistory[_number];
+//     }
+
+//     function vehicleOwnershipTransfer(
+//         string memory _number,
+//         address _newOwner
+//     ) public onlyRegisteredUser {
+//         Vehicle storage vehicle = vehicles[_number];
+//         require(
+//             msg.sender == vehicle.owner,
+//             "Only the current owner can transfer vehicle ownership"
+//         );
+
+//         ownershipHistory[_number].push(vehicles[_number].owner);
+//         vehicles[_number].owner = _newOwner;
+//     }
+// }
+
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
@@ -24,6 +168,8 @@ contract VehicleManagement {
     }
 
     struct Vehicle {
+        string ownerName;
+        string contact;
         string model;
         string color;
         string number;
@@ -33,26 +179,19 @@ contract VehicleManagement {
         RequestStatus status;
     }
 
-    struct VehicleRequest {
-        string number;
-        string model;
-        string color;
-        string category;
-        string chassisNumber;
-        address requester;
-        RequestStatus status;
-    }
-
     struct OwnershipRequest {
         string number;
         address newOwner;
-        address requester;
+        string newOwnerName;
+        string newOwnerContact;
+        string previousOwnerName;
+        string previousOwnerContact;
+        address previousOwnerAddress;
     }
 
     mapping(address => User) public users;
     mapping(string => Vehicle) public vehicles;
-    mapping(string => address[]) public ownershipHistory;
-    mapping(string => VehicleRequest) public vehicleRequests;
+    mapping(string => OwnershipRequest[]) public ownershipHistory;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can perform this action");
@@ -71,6 +210,8 @@ contract VehicleManagement {
     }
 
     function requestVehicleRegistration(
+        string memory _ownerName,
+        string memory _contact,
         string memory _number,
         string memory _model,
         string memory _color,
@@ -83,6 +224,8 @@ contract VehicleManagement {
         );
 
         Vehicle memory newRequest = Vehicle({
+            ownerName: _ownerName,
+            contact: _contact,
             number: _number,
             model: _model,
             color: _color,
@@ -129,13 +272,15 @@ contract VehicleManagement {
 
     function getOwnershipHistory(
         string memory _number
-    ) public view returns (address[] memory) {
+    ) public view returns (OwnershipRequest[] memory) {
         return ownershipHistory[_number];
     }
 
     function vehicleOwnershipTransfer(
         string memory _number,
-        address _newOwner
+        address _newOwner,
+        string memory _newOwnerName,
+        string memory _newOwnerContact
     ) public onlyRegisteredUser {
         Vehicle storage vehicle = vehicles[_number];
         require(
@@ -143,7 +288,19 @@ contract VehicleManagement {
             "Only the current owner can transfer vehicle ownership"
         );
 
-        ownershipHistory[_number].push(vehicles[_number].owner);
+        OwnershipRequest memory transferRequest = OwnershipRequest({
+            number: _number,
+            newOwner: _newOwner,
+            newOwnerName: _newOwnerName,
+            newOwnerContact: _newOwnerContact,
+            previousOwnerName: vehicle.ownerName,
+            previousOwnerContact: vehicle.contact,
+            previousOwnerAddress: vehicle.owner
+        });
+
+        ownershipHistory[_number].push(transferRequest);
         vehicles[_number].owner = _newOwner;
+        vehicles[_number].ownerName = _newOwnerName;
+        vehicles[_number].contact = _newOwnerContact;
     }
 }
--- smartcontract/test/VehicleManagement.test.js
+++ smartcontract/test/VehicleManagement.test.js
@@ -29,53 +29,69 @@ describe("VehicleManagement Smart Contract", function () {
   it("requestVehicleRegistration", async function () {
     await vehicleManagement
       .connect(a1)
-      .requestVehicleRegistration("8030", "2000", "black", "bike", "12345678");
-    // console.log(await vehicleManagement.connect(a1).vehicles("8030"));
+      .requestVehicleRegistration(
+        "Zeeshan Nawaz",
+        "0300xx",
+        "8030",
+        "2000",
+        "black",
+        "bike",
+        "12345678"
+      );
   });
   it("approveVehicleRegistration", async function () {
     await vehicleManagement.connect(owner).approveVehicleRegistration("8030");
-    // console.log(await vehicleManagement.connect(a1).vehicles("8030"));
   });
-  // it("rejectVehicleRegistration", async function () {
-  //   await vehicleManagement.connect(owner).rejectVehicleRegistration("8030");
-  //   // console.log(await vehicleManagement.connect(a1).vehicles("8030"));
-  // });
+
   it("requestVehicleRegistration", async function () {
     await vehicleManagement
       .connect(a1)
-      .requestVehicleRegistration("8040", "2000", "black", "bike", "12345678");
+      .requestVehicleRegistration(
+        "Arsalan Nawaz",
+        "0310xx",
+        "8040",
+        "2000",
+        "black",
+        "bike",
+        "12345678"
+      );
   });
 
   it("approveVehicleRegistration", async function () {
     await vehicleManagement.connect(owner).approveVehicleRegistration("8040");
   });
 
-  it("getAllVehicles", async function () {
-    console.log(await vehicleManagement.connect(a1).getAllVehicles());
+  // it("getAllVehicles", async function () {
+  //   console.log(await vehicleManagement.connect(a1).getAllVehicles());
+  // });
+  it("vehicleOwnershipTransfer", async function () {
+    await vehicleManagement
+      .connect(a1)
+      .vehicleOwnershipTransfer("8030", a2.getAddress(), "Haris", "0900");
   });
-  // it("getAllVehicleRequests", async function () {
-  //   console.log(await vehicleManagement.connect(a1).getAllVehicleRequests());
-  // });
-
-  // it("vehicleOwnershipTransfer", async function () {
-  //   await vehicleManagement
-  //     .connect(a1)
-  //     .vehicleOwnershipTransfer("8030", a2.getAddress());
-  //   console.log(await vehicleManagement.connect(a1).vehicles("8030"));
-  // });
-  // it("vehicleOwnershipTransfer", async function () {
-  //   await vehicleManagement
-  //     .connect(a2)
-  //     .vehicleOwnershipTransfer("8030", a3.getAddress());
-  //   console.log(await vehicleManagement.connect(a2).vehicles("8030"));
-  // });
-
-  // it("ownershipHistory", async function () {
-  //   console.log(
-  //     await vehicleManagement.connect(a1).ownershipHistory("8030", 0)
-  //   );
-  //   console.log(
-  //     await vehicleManagement.connect(a1).ownershipHistory("8030", 1)
-  //   );
-  // });
+  it("vehicleOwnershipTransfer", async function () {
+    await vehicleManagement
+      .connect(a2)
+      .vehicleOwnershipTransfer("8030", a3.getAddress(), "Sohail", "9113");
+  });
+  it("vehicleOwnershipTransfer", async function () {
+    await vehicleManagement
+      .connect(a3)
+      .vehicleOwnershipTransfer("8030", a4.getAddress(), "Sohail", "9113");
+  });
+  it("getOwnershipHistory", async function () {
+    console.log(
+      "a1",
+      a1.getAddress(),
+      "a2",
+      a2.getAddress(),
+      "a3",
+      a3.getAddress(),
+      "a4",
+      a4.getAddress()
+    );
+    console.log(
+      await vehicleManagement.connect(a1).getOwnershipHistory("8030")
+    );
+  });
 });
